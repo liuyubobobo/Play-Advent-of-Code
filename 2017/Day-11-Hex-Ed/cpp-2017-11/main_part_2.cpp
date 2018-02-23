1f772a3a03dcda7cf6e7e7fa7cb9b1060e12a298
liuyubobobo <liuyubobobo@gmail.com>
2018-02-23 01:30:54 -0800

    2017 day11 solved.

---
+++ 2017/Day-11-Hex-Ed/cpp-2017-11/main_part_2.cpp
@@ -0,0 +1,74 @@
+/// http://adventofcode.com/2017/day/11
+/// liuyubobobo
+/// 2018-02-23
+
+#include <iostream>
+#include <fstream>
+#include <cassert>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+
+
+class Solution{
+
+public:
+    int furthest_distance(const vector<string>& paths){
+
+        int x = 0, y = 0;
+        int res = 0;
+        for(const string& direction: paths){
+            if(direction == "n")
+                x -= 1;
+            else if(direction == "ne")
+                y += 1;
+            else if(direction == "se")
+                x += 1, y += 1;
+            else if(direction == "s")
+                x += 1;
+            else if(direction == "sw")
+                y -= 1;
+            else if(direction == "nw")
+                x -= 1, y -= 1;
+            else
+                assert(false);
+
+            res = max(res, fewest_distance(x, y));
+        }
+
+        return res;
+    }
+
+private:
+    int fewest_distance(int x, int y){
+        int res1 = abs(x) + abs(y-x);
+        int res2 = abs(y) + abs(x-y);
+        return min(res1, res2);
+    }
+};
+
+int main() {
+
+    ifstream input;
+    input.open("../input.txt");
+    assert(input.is_open());
+
+    string line;
+    getline(input, line);
+    for(int i = 0 ; i < line.size() ; i ++)
+        if(line[i] == ',')
+            line[i] = ' ';
+
+    stringstream ss(line);
+    vector<string> paths;
+    string direction;
+    while(ss >> direction)
+        paths.push_back(direction);
+
+    cout << Solution().furthest_distance(paths) << endl;
+
+    input.close();
+
+    return 0;
+}
