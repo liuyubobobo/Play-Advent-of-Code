428291b1b92d45096a942f03bb4cbf8321225a6b
Yubo Liu <liuyubobobo@gmail.com>
2020-12-02 01:50:09 -0800

    2015 day 02 solved.

---
+++ 2015/Day-02-I-Was-Told-There-Would-Be-No-Math/cpp-2015-02/main_part_1.cpp
@@ -0,0 +1,50 @@
+/// Source : https://adventofcode.com/2015/day/2
+/// Author : liuyubobobo
+/// Time   : 2020-12-02
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cassert>
+
+using namespace std;
+
+
+class Solution{
+
+public:
+    int solve(const vector<int>& v){
+
+        return 2 * (v[0] * v[1] + v[1] * v[2] + v[0] * v[2]) +
+               min(v[0] * v[1], min(v[1] * v[2], v[0] * v[2]));
+    }
+};
+
+int main() {
+
+    ifstream input;
+    input.open("../../input.txt");
+    assert(input.is_open());
+
+    string s;
+    int res = 0;
+    while(!input.eof()) {
+
+        input >> s;
+
+        vector<int> v;
+        for (int start = 0, i = start + 1; i <= s.size(); i++)
+            if (i == s.size() || s[i] == 'x') {
+                v.push_back(atoi(s.substr(start, i - start).c_str()));
+                start = i + 1;
+                i = start;
+            }
+        assert(v.size() == 3);
+        res += Solution().solve(v);
+    }
+
+    cout << res << endl;
+    input.close();
+
+    return 0;
+}
--- 2015/readme.md
+++ 2015/readme.md
@@ -3,4 +3,5 @@
 | Problem | Link | C++ | Java | Python |
 | --- | :---: | :---: | :---: | :---: |
 | Day 1: Not Quite Lisp | [Link](https://adventofcode.com/2015/day/1) | [C++](Day-01-Not-Quite-Lisp/cpp-2015-01/) | | |
+| Day 2: I Was Told There Would Be No Math | [Link](https://adventofcode.com/2015/day/2) | [C++](Day-02-I-Was-Told-There-Would-Be-No-Math/cpp-2015-02/) | | |
 | | | | | |
